0db68b7202cbe464dd402200f0901ccd315801fe
Jim Kalafut <jim@kalafut.net>
2022-09-07 13:41:51 -0700

    Use pattern matching

--- dateify.py
+++ dateify.py
@@ -1,40 +1,30 @@
 import os
+import glob
+from enum import Enum, auto
 
-files = []
-with os.scandir(".") as it:
-    for entry in it:
-        if entry.name.endswith('.md') and entry.is_file():
-            files.append(entry.name)
+class HeaderState(Enum):
+    NOT_STARTED = auto()
+    STARTED = auto()
+    ENDED = auto()
 
-for fn in files[:2]:
-    header_started = False
-    header_finished = False
+for fn in glob.glob("*.md"):
+    header_state = HeaderState.NOT_STARTED
     valid = False
-    changed = False
     new_lines = []
 
     with open(fn) as f:
         for line in f.readlines():
-            if line.startswith("date:"):
-                valid = True
-                break
-
-            if line.startswith("---") and not header_finished:
-                if not header_started:
-                    header_started = True
-                else:
-                    post_date = fn[:10]
-                    new_lines.append(f"date: {post_date}\n")
-                    header_finished = True
+            match [line[:5].strip(), header_state]:
+                case ["date:", *_]:
+                    valid = True
+                case ["---", HeaderState.NOT_STARTED]:
+                    header_state = HeaderState.STARTED
+                case ["---", HeaderState.STARTED]:
+                    new_lines.append(f"date: {fn[:10]}\n")
+                    header_state = HeaderState.ENDED
 
             new_lines.append(line)
 
     if not valid:
         with open(fn, "w") as f:
             f.writelines(new_lines)
-
-#    if valid:
-#        print(f"No updated needed for {fn}")
-
-
-#print(files)
